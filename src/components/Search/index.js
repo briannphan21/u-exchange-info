a8b78752a19318f8e7f428d62eb092ad61535b10
briannphan21 <61403034+briannphan21@users.noreply.github.com>
2021-02-17 01:19:11 -0500

    Update index.js

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -11,7 +11,7 @@ import { useAllPairData, usePairData } from '../../contexts/PairData'
 import DoubleTokenLogo from '../DoubleLogo'
 import { useMedia } from 'react-use'
 import { useAllPairsInUniswap, useAllTokensInUniswap } from '../../contexts/GlobalData'
-import { TOKEN_BLACKLIST, PAIR_BLACKLIST } from '../../constants'
+import { OVERVIEW_TOKEN_BLACKLIST, PAIR_BLACKLIST } from '../../constants'
 
 import { transparentize } from 'polished'
 import { client } from '../../apollo/client'
@@ -273,6 +273,12 @@ export const Search = ({ small = false }) => {
     return uniqueTokens
       ? uniqueTokens
           .sort((a, b) => {
+            if (OVERVIEW_TOKEN_BLACKLIST.includes(a.id)) {
+              return 1
+            }
+            if (OVERVIEW_TOKEN_BLACKLIST.includes(b.id)) {
+              return -1
+            }
             const tokenA = allTokenData[a.id]
             const tokenB = allTokenData[b.id]
             if (tokenA?.oneDayVolumeUSD && tokenB?.oneDayVolumeUSD) {
@@ -287,7 +293,7 @@ export const Search = ({ small = false }) => {
             return 1
           })
           .filter((token) => {
-            if (TOKEN_BLACKLIST.includes(token.id)) {
+           if (OVERVIEW_TOKEN_BLACKLIST.includes(token.id))  {
               return false
             }
             const regexMatches = Object.keys(token).map((tokenEntryKey) => {

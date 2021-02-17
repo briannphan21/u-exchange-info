f9fa515d921716e7132cec1ff8156e129657088c
briannphan21 <61403034+briannphan21@users.noreply.github.com>
2021-02-17 01:41:21 -0500

    Update index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -87,14 +87,6 @@ export const toNiceDate = (date) => {
   return x
 }
 
-// shorten the checksummed version of the input address to have 0x + 4 characters at start and end
-export function shortenAddress(address, chars = 4) {
-  const parsed = isAddress(address)
-  if (!parsed) {
-    throw Error(`Invalid 'address' parameter '${address}'.`)
-  }
-  return `${parsed.substring(0, chars + 2)}...${parsed.substring(42 - chars)}`
-}
 
 export const toWeeklyDate = (date) => {
   const formatted = dayjs.utc(dayjs.unix(date))
